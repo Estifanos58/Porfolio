6443be024fb4c5fe526eae7b6a80e84614b70e4d
Estifanos58 <122863605+Estifanos58@users.noreply.github.com>
2025-03-26 10:08:47 +0300

    dynamics in the making

--- src/components/HeroOrbit.tsx
+++ src/components/HeroOrbit.tsx
@@ -1,19 +1,51 @@
 import { PropsWithChildren } from "react";
+import { twMerge } from "tailwind-merge";
 
-function HeroOrbit({ children, size,  rotation}: PropsWithChildren<{ size: number, rotation:number }>) {
+function HeroOrbit({
+  children,
+  size,
+  rotation,
+  orbitDuration,
+  shouldOrbit = false,
+  shouldSpin = false,
+  spinDuration,
+}: PropsWithChildren<{
+  size: number;
+  rotation: number;
+  orbitDuration?: string;
+  shouldOrbit?: boolean;
+  shouldSpin?: boolean;
+  spinDuration?: string;
+}>) {
   return (
     <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
       <div
-        className="flex items-start justify-start"
+        className={twMerge(shouldOrbit && "animate-spin")}
         style={{
-          height: `${size}px`,
-          width: `${size}px`,
-          transform: `rotate(${rotation}deg)`,
+          animationDuration: orbitDuration,
         }}
       >
-        <div className=" inline-flex" style={{
-            transform: `rotate(${rotation * -1}deg)`
-        }}>{children}</div>
+        <div
+          className="flex items-start justify-start"
+          style={{
+            height: `${size}px`,
+            width: `${size}px`,
+            transform: `rotate(${rotation}deg)`,
+          }}
+        >
+          <div className={twMerge(shouldSpin && "animate-spin")} style={{
+            animationDuration: spinDuration || '10s'
+          }}>
+            <div
+              className=" inline-flex"
+              style={{
+                transform: `rotate(${rotation * -1}deg)`,
+              }}
+            >
+              {children}
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -25,7 +25,7 @@ export const Footer = () => {
   return <footer className='relative overflow-x-clip -z-10'>
     <div className="absolute h-[400px] w-[1600px] bottom-0 left-1/2 -translate-x-1/2 bg-emerald-300/30 -z-10 [mask-image:radial-gradient(50%_50%_at_bottom_center,black,transparent)]"></div>
     <div className="container">
-      <div className='border-t border-white/15 py-6 text-sm flex flex-col gap-8 md:flex-row md:justify-betweenitems-center'>
+      <div className='border-t border-white/15 py-6 text-sm flex flex-col gap-8 md:flex-row md:justify-between items-center'>
         <div className='text-white/40 '>&copy;{` ${year}. All rights reserved`}</div>
         <nav className='flex flex-col md:flex-row items-center gap-8'>
           {
