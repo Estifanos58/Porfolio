19344eedf25645b92cd304f848f38015672792d0
Estifanos58 <122863605+Estifanos58@users.noreply.github.com>
2025-03-30 12:39:20 +0300

    email sending in progress

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -1,10 +1,39 @@
-import ArrowUpRightIcon from "@/assets/icons/arrow-up-right.svg";
+'use client'
+import { useState } from "react";
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button";
 import { Textarea } from "@/components/ui/textarea";
 import grainImage from '@/assets/images/grain.jpg'
+import axios from "axios";
 
 export const ContactSection = () => {
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState(""); 
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  const handleSubmit = async ()=>{
+    if(!email || !name || !message) {
+      return setError("All forms must be submited before sending");
+    }
+    setIsLoading(true);
+    const response = await axios.post("/api/sendMessage",{name, email, message});
+
+    if(response.status === 200){
+      setIsLoading(false);
+      setName("");
+      setEmail("");
+      setMessage("");
+      alert("Message sent successfully");
+    }else{
+      setIsLoading(false);
+      setError("Failed to send message");
+    }
+
+  }
+
   return (
     <div id="contact" className="py-16 pt-12 lg:py-24 lg:pt-20 ">
       <div className="container">
@@ -23,10 +52,11 @@ export const ContactSection = () => {
             </div>  
 
             <div className="flex flex-col justify-center items-center gap-4 md:gap-2 md:text-sm w-full">
-              <Input type="text" placeholder="Enter Your Name" className="p-2 md:p-1 w-full font-mono rounded-md" />
-              <Input type="text" placeholder="Enter Your Email" className="p-2 rounded-md font-mono md:p-1 w-full" />
-              <Textarea placeholder="Enter Your Message" className="p-2 rounded-md w-full h-36 md:h-20 font-mono resize-none"/>
-              <Button className="px-6 py-2 md:py-1 text-xl rounded-lg text-white w-[200px] font-mono">Send</Button>
+              <Input value={name} onChange={(e)=>setName(e.target.value)} type="text" placeholder="Enter Your Name" className="p-2 md:p-1 w-full font-mono rounded-md" />
+              <Input value={email} onChange={(e)=> setEmail(e.target.value)} type="text" placeholder="Enter Your Email" className="p-2 rounded-md font-mono md:p-1 w-full" />
+              <Textarea value={message} onChange={(e)=> setMessage(e.target.value)} placeholder="Enter Your Message" className="p-2 rounded-md w-full h-36 md:h-20 font-mono resize-none"/>
+                {error && <p className="text-red-500 text-sm">{error}</p>}
+              <Button onClick={()=>handleSubmit()} className="px-6 py-2 md:py-1 text-xl rounded-lg text-white w-[200px] font-mono">{isLoading ?"Loading":`Send`}</Button>
             </div>
 
           </div>
